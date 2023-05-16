dbb30cf16d5e8e4316240079de40ab2c57524cd0
AakibAnsari <aakiba6619@gmail.com>
2023-05-16 21:34:09 +0530

    Text-Utils-master

---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import pic from '../images/pic.jpg';
+// import '../App.css';
+
+export default function About() {
+
+
+  return (
+    <>
+        <div className="container">
+            <div className="heading">
+                <h2>About Me</h2>
+            </div>
+            <div className="details">
+                <div className="image">
+                    <img src={pic} alt="" />
+                </div>
+                <div className="information">
+                <h2>MD Aakib Ansari</h2><span>Full Stack Developer</span>
+                </div>
+            </div>
+        </div>
+    </>
+    
+  )
+}
